4ab5e350a08bb92959091f034d734d9b54170d1b
Olga Golovkina <olgadietology@gmail.com>
2017-06-05 21:56:22 +0300

    Add servers test

--- servers_test.go
+++ servers_test.go
@@ -1,25 +1,58 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+	//"net/http"
+	//"net/http/httptest"
+	//"testing"
+	//
+	//"github.com/gorilla/mux"
+	//"github.com/stretchr/testify/assert"
+	"net"
 
-	"github.com/gorilla/mux"
-	"github.com/stretchr/testify/assert"
+	"testing"
+	"fmt"
+	"bytes"
+	"log"
+	"os"
 )
 
-func Router() *mux.Router {
+//func Router() *mux.Router {
+//
+//	r := mux.NewRouter()
+//	r.HandleFunc("/devices/{id}", webSocketHandler)
+//	r.HandleFunc("/devWS", webSocketHandler)
+//	return r
+//}
+//
+//func TestCreateEndpoint(t *testing.T) {
+//	request, _ := http.NewRequest("GET", "/devices", nil)
+//	response := httptest.NewRecorder()
+//	Router().ServeHTTP(response, request)
+//	assert.Equal(t, 200, response.Code, "OK response is expected")
+//}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/devices/{id}", webSocketHandler)
-	r.HandleFunc("/devWS", webSocketHandler)
-	return r
-}
+func TestSendDefaultConfiguration(t *testing.T) {
 
-func TestCreateEndpoint(t *testing.T) {
-	request, _ := http.NewRequest("GET", "/devices", nil)
-	response := httptest.NewRecorder()
-	Router().ServeHTTP(response, request)
-	assert.Equal(t, 200, response.Code, "OK response is expected")
+	var pool ConectionPool
+	pool.init()
+
+	//ln, err := net.Listen(connType, "localhost"+":"+"3000")
+	//fmt.Println(ln,',',err)
+	//for {
+	//	conn, err := ln.Accept()
+	//	fmt.Println(conn,',',err)
+	//}
+	//conn, err := ln.Accept()
+	var conn net.Conn
+	fmt.Println(conn)
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+	sendDefaultConfiguration(&conn, &pool)
+	t.Log(buf.String())
+
+
 }
