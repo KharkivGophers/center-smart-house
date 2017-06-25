b0941cb81298194db59343c9e670b81ade1bc9a9
Ihor Bondartsov <bondartsov.igor@gmail.com>
2017-06-25 20:51:46 +0300

    Some change in dao. Deleted all global variable.

--- handlers.go
+++ handlers.go
@@ -11,7 +11,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"menteslibres.net/gosexy/redis"
-	"fmt"
 )
 
 //--------------------TCP-------------------------------------------------------------------------------------
