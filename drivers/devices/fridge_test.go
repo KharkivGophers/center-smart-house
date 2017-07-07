f7b4221a863edeb4ab9db00e06d2fca7456d3a9d
IhorBondartsov <bondartsov.igor@mail.ru>
2017-07-07 15:08:23 +0300

    test with mock

--- drivers/devices/fridge_test.go
+++ drivers/devices/fridge_test.go
@@ -8,60 +8,76 @@ import (
 
 
 	"reflect"
-	"strconv"
+	//"strconv"
 )
 
 // ------------------------With Mock--------------------------------------------------------------
-func TestSetDevConfig(t *testing.T){
-	dbMock := dao.DBMock{&dao.DbMockClient{}}
+//func TestSetDevConfig(t *testing.T){
+//	dbMock := dao.DBMock{&dao.DbMockClient{}}
+//	fridge := Fridge{}
+//	devConfig := models.DevConfig{true,true,
+//		int64(0),int64(0),"mac"}
+//
+//	Convey("Should be all ok", t, func() {
+//		fridge.SetDevConfig("test", &devConfig, dbMock.Client)
+//		mustBe := " TurnedOn true CollectFreq 0 SendFreq 0 StreamOn true"
+//
+//		var actual string
+//
+//		for _, val := range dbMock.Client.Hash["test"]{
+//			arr:=val.([]interface{})
+//			for _, val := range arr{
+//				switch reflect.TypeOf(val).String() {
+//				case "bool":
+//					actual += " " + strconv.FormatBool(val.(bool))
+//				case "string":
+//					actual += " " + val.(string)
+//				case "int64":
+//					actual += " " + strconv.FormatInt(val.(int64),10)
+//				}
+//			}
+//		}
+//		So(actual, ShouldEqual, mustBe)
+//	})
+//}
+//
+//func TestGetDevConfig(t *testing.T){
+//	dbMock := dao.DBMock{&dao.DbMockClient{}}
+//	fridge := Fridge{}
+//	devConfig := models.DevConfig{true,true,
+//				      int64(0),int64(0),"00-00-00-11-11-11"}
+//
+//	Convey("Should be all ok", t, func() {
+//		fridge.SetDevConfig("test", &devConfig, dbMock.Client)
+//		config := fridge.GetDevConfig("test", "00-00-00-11-11-11", dbMock.Client)
+//		So(reflect.DeepEqual(*config, devConfig), ShouldBeTrue)
+//	})
+//}
+//
+//
+
+
+//------------------------------------------------------------
+func TestSetDevConfigWithRedis(t *testing.T){
+	dbWorker := dao.RedisClient{DbServer:models.Server{IP:"0.0.0.0", Port: uint(6379)}}
+	dbWorker.Connect()
 	fridge := Fridge{}
 	devConfig := models.DevConfig{true,true,
-		int64(0),int64(0),"mac"}
+				      int64(200),int64(200),"00-00-00-11-11-11"}
 
 	Convey("Should be all ok", t, func() {
-		fridge.SetDevConfig("test", &devConfig, dbMock.Client)
-		mustBe := " TurnedOn true CollectFreq 0 SendFreq 0 StreamOn true"
-
-		var actual string
-
-		for _, val := range dbMock.Client.Hash["test"]{
-			arr:=val.([]interface{})
-			for _, val := range arr{
-				switch reflect.TypeOf(val).String() {
-				case "bool":
-					actual += " " + strconv.FormatBool(val.(bool))
-				case "string":
-					actual += " " + val.(string)
-				case "int64":
-					actual += " " + strconv.FormatInt(val.(int64),10)
-				}
-			}
-		}
-		So(actual, ShouldEqual, mustBe)
+		fridge.SetDevConfig("00-00-00-11-11-11:config", &devConfig, dbWorker.Client)
+		isConfig := fridge.GetDevConfig("00-00-00-11-11-11:config","00-00-00-11-11-11", dbWorker.Client)
+		So(reflect.DeepEqual(*isConfig, devConfig), ShouldBeTrue)
 	})
 }
-
-func TestGetDevConfig(t *testing.T){
-	dbMock := dao.DBMock{&dao.DbMockClient{}}
-	fridge := Fridge{}
-	devConfig := models.DevConfig{true,true,
-				      int64(0),int64(0),"00-00-00-11-11-11"}
-
-	Convey("Should be all ok", t, func() {
-		fridge.SetDevConfig("test", &devConfig, dbMock.Client)
-		config := fridge.GetDevConfig("test", "00-00-00-11-11-11", dbMock.Client)
-		So(reflect.DeepEqual(*config, devConfig), ShouldBeTrue)
-	})
-}
-
-
 // Impossible to testing StreamOn and TurnedOn
 func TestValidateDevData(t *testing.T){
 	fridge := Fridge{}
 	var devConfig models.DevConfig
 	Convey("Invalid MAC. Should be false", t, func() {
 		devConfig = models.DevConfig{true,true,
-					      int64(0),int64(0),"Invalid mac"}
+					     int64(0),int64(0),"Invalid mac"}
 		valid, _ :=fridge.ValidateDevData(devConfig)
 		So(valid, ShouldBeFalse)
 	})
@@ -84,6 +100,3 @@ func TestValidateDevData(t *testing.T){
 		So(valid, ShouldBeFalse)
 	})
 }
-
-//------------------------------------------------------------
-
